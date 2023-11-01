496d12f20c14d2448b239baf6f1864156b8e8619
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-11-01 15:28:06 -0300

    Added colours to map

--- TextBasedRPGMap/Program.cs
+++ TextBasedRPGMap/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +9,48 @@ namespace TextBasedRPGMap
 {
     internal class Program
     {
-        static ConsoleColor currentBackground = ConsoleColor.Black;
+        static ConsoleColor currentBackground;
         static void Main(string[] args)
+        {
+            DisplayMap(3);
+        }
+        static void DisplayMap(int s)
         {
             for (int x = 0; x < 12; x++)
             {
                 for (int y = 0; y < 30; y++)
                 {
-                    Console.Write(map[x, y]);
+                    for(int z = 0; z < s; z++)
+                    {
+                        if (map[x, y] == '`')
+                        {
+                            Console.BackgroundColor = ConsoleColor.Green;
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            Console.Write(map[x, y]);
+                        }
+                        else if (map[x, y] == '~')
+                        {
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            Console.ForegroundColor = ConsoleColor.DarkBlue;
+                            Console.Write(map[x, y]);
+                        }
+                        else if (map[x, y] == '^')
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.Write(map[x, y]);
+                        }
+                        else if (map[x, y] == '*')
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGreen;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(map[x, y]);
+                        }
+                    }
                 }
                 Console.WriteLine();
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
             }
         }
         static char[,] map = new char[,] // dimensions defined by following data:
@@ -35,33 +68,6 @@ namespace TextBasedRPGMap
         {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
         {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
     };
-        static void changeBackgroundColor()
-        {
-            foreach (char c in map)
-            {
-                if (c == '^')
-                {
-                    Console.BackgroundColor = ConsoleColor.Gray;
-                }
-                else if (c == '`')
-                {
-                    Console.BackgroundColor = ConsoleColor.Green;
-                }
-                else if (c == '~')
-                {
-                    Console.BackgroundColor = ConsoleColor.Blue;
-                }
-                else if (c == '*')
-                {
-                    Console.BackgroundColor = ConsoleColor.DarkGreen;
-                }
-                else
-                {
-                    Console.BackgroundColor = ConsoleColor.Black;
-                }
-            }
-        }
-
         // map legend:
         // ^ = mountain
         // ` = grass
