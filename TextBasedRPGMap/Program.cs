96ddbee0dcd0e99f7ed82289f765729bd70bcd5f
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-11-06 13:28:20 -0400

    Added colours and ASCII

--- TextBasedRPGMap/Program.cs
+++ TextBasedRPGMap/Program.cs
@@ -9,49 +9,107 @@ namespace TextBasedRPGMap
 {
     internal class Program
     {
-        static ConsoleColor currentBackground;
+        static Encoding asciiEncoder = Encoding.GetEncoding("IBM437");
+        static string water = ($"{asciiEncoder.GetString(new byte[1] { 176 })}");
+        static string grass = ($"{asciiEncoder.GetString(new byte[1] { 177 })}");
+        static string tree = ($"{asciiEncoder.GetString(new byte[1] { 178 })}");
+        static string borderHorizontal = ($"{asciiEncoder.GetString(new byte[1] { 205 })}");
+        static string borderVertical = ($"{asciiEncoder.GetString(new byte[1] { 186 })}");
+        static string borderTL = ($"{asciiEncoder.GetString(new byte[1] { 214 })}");
+        static string borderTR = ($"{asciiEncoder.GetString(new byte[1] { 184 })}");
+        static string borderBL = ($"{asciiEncoder.GetString(new byte[1] { 212 })}");
+        static string borderBR = ($"{asciiEncoder.GetString(new byte[1] { 189 })}");
+        static char mountain = Convert.ToChar(30);
         static void Main(string[] args)
         {
-            DisplayMap(3);
+            DisplayBorderTop(1);
+            DisplayMap(1);
+            DisplayBorderBottom(1);
+            Console.WriteLine("DisplayMap(1)");
+            DisplayLegend();
+            DisplayBorderTop(2);
+            DisplayMap(2);
+            DisplayBorderBottom(2);
+            Console.WriteLine("DisplayMap(2)");
+            DisplayLegend();
         }
         static void DisplayMap(int s)
         {
             for (int x = 0; x < 12; x++)
             {
+                Console.Write(borderVertical);
                 for (int y = 0; y < 30; y++)
                 {
                     for(int z = 0; z < s; z++)
                     {
                         if (map[x, y] == '`')
                         {
-                            Console.BackgroundColor = ConsoleColor.Green;
-                            Console.ForegroundColor = ConsoleColor.DarkGreen;
-                            Console.Write(map[x, y]);
+                            ColorChange(ConsoleColor.Green, ConsoleColor.DarkGreen);
+                            Console.Write(grass);
                         }
                         else if (map[x, y] == '~')
                         {
-                            Console.BackgroundColor = ConsoleColor.Blue;
-                            Console.ForegroundColor = ConsoleColor.DarkBlue;
-                            Console.Write(map[x, y]);
+                            ColorChange(ConsoleColor.DarkCyan, ConsoleColor.White);
+                            Console.Write(water);
                         }
                         else if (map[x, y] == '^')
                         {
-                            Console.BackgroundColor = ConsoleColor.DarkGray;
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            Console.Write(map[x, y]);
+                            ColorChange(ConsoleColor.DarkGray, ConsoleColor.Gray);
+                            Console.Write(mountain);
                         }
                         else if (map[x, y] == '*')
                         {
-                            Console.BackgroundColor = ConsoleColor.DarkGreen;
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.Write(map[x, y]);
+                            ColorChange(ConsoleColor.Green, ConsoleColor.DarkGreen);
+                            Console.Write(tree);
                         }
                     }
                 }
-                Console.WriteLine();
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.ForegroundColor = ConsoleColor.White;
+                ColorChange(ConsoleColor.Black, ConsoleColor.White);
+                Console.Write(borderVertical);
+                Console.Write("\n");
             }
+        }
+        static void DisplayBorderTop(int s)
+        {
+            Console.Write(borderTL);
+            for (int i = 0; i < 30*s; i++)
+            {
+                Console.Write(borderHorizontal);
+            }
+            Console.Write(borderTR);
+            ColorChange(ConsoleColor.Black, ConsoleColor.White);
+            Console.WriteLine();
+        }
+        static void DisplayBorderBottom(int s)
+        {
+            Console.Write(borderBL);
+            for (int i = 0; i < 30 * s; i++)
+            {
+                Console.Write(borderHorizontal);
+            }
+            Console.Write(borderBR);
+            ColorChange(ConsoleColor.Black, ConsoleColor.White);
+            Console.WriteLine();
+        }
+        static void DisplayLegend()
+        {
+            Console.Write("\nLegend:\nGrass: ");
+            ColorChange(ConsoleColor.Green, ConsoleColor.DarkGreen);
+            Console.Write(grass);
+            ColorChange(ConsoleColor.Black, ConsoleColor.White);
+            Console.Write("\nWater: ");
+            ColorChange(ConsoleColor.DarkCyan, ConsoleColor.White);
+            Console.Write(water);
+            ColorChange(ConsoleColor.Black, ConsoleColor.White);
+            Console.Write("\nMountain: ");
+            ColorChange(ConsoleColor.DarkGray, ConsoleColor.Gray);
+            Console.Write(mountain);
+            ColorChange(ConsoleColor.Black, ConsoleColor.White);
+            Console.Write("\nTree: ");
+            ColorChange(ConsoleColor.Green, ConsoleColor.DarkGreen);
+            Console.Write(tree);
+            ColorChange(ConsoleColor.Black, ConsoleColor.White);
+            Console.WriteLine();
         }
         static char[,] map = new char[,] // dimensions defined by following data:
     {
@@ -68,6 +126,11 @@ namespace TextBasedRPGMap
         {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
         {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
     };
+        static void ColorChange(ConsoleColor background, ConsoleColor foreground)
+        {
+            Console.BackgroundColor = background;
+            Console.ForegroundColor = foreground;
+        }
         // map legend:
         // ^ = mountain
         // ` = grass
